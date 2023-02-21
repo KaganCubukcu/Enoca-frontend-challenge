eae301dbc2715dfd442f7ee0dd51bc512c756055
Kağan Çubukçu <kagancancubukcu@gmail.com>
2023-02-21 14:47:52 +0300

    Deleted unnecessary paths and added a new path

--- src/App.js
+++ src/App.js
@@ -1,61 +1,37 @@
-import Home from "./pages/home/Home";
-import { createBrowserRouter } from "react-router-dom";
-import { RouterProvider } from "react-router";
-import Business from "./pages/business/Business";
+import Home from "./pages/Home";
 import About from "./pages/about/About";
-import General from "./pages/general/General";
-import Health from "./pages/health/Health";
-import Entertainment from "./pages/entertainment/Entertainment";
-import Science from "./pages/science/Science";
-import Sports from "./pages/sports/Sports";
-import Technology from "./pages/technology/Technology";
-import Categories from "./pages/categories/Categories";
+import Categories from "./pages/Categories";
+import CategoryDetails from "./pages/CategoryDetails";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function App() {
+  const selectedCategory = useSelector(
+    (state) => state.filterSlice.selectedCategory
+  );
+
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Home />,
     },
     {
-      path: "/business",
-      element: <Business />,
-    },
-    {
       path: "/about",
       element: <About />,
     },
     {
-      path: "/general",
-      element: <General />,
+      path: `/:${selectedCategory}`,
+      element: <CategoryDetails />,
     },
-    {
-      path: "/health",
-      element: <Health />,
-    },
-    {
-      path: "/entertainment",
-      element: <Entertainment />,
-    },
-    {
-      path: "/science",
-      element: <Science />,
-    },
-    {
-      path: "/sports",
-      element: <Sports />,
-    },
-    {
-      path: "/technology",
-      element: <Technology />,
-    },
+
     {
       path: "/categories",
       element: <Categories />,
     },
   ]);
   return (
-    <div style={{}}>
+    <div>
       <RouterProvider router={router} />
     </div>
   );
